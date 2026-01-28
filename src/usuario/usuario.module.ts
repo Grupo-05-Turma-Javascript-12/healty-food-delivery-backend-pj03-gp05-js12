cd2063b15a63ea647a368d1a1f75543022c6a20c
João Henrique <jhenrique.caval@hotmail.com>
2026-01-28 11:18:49 -0300

    feat: colocando forwardRef para o AuthModule

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioController } from './controllers/user.controller';
 import { Usuario } from './entities/user.entity';
 import { UsuarioService } from './services/user.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [TypeOrmModule.forFeature([Usuario]), forwardRef(() => AuthModule)],
   providers: [UsuarioService],
   controllers: [UsuarioController],
   exports: [UsuarioService],

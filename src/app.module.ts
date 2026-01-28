eecc53d00fb207906b08ff8071e6ac1e83a045cf
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-28 15:16:20 -0300

    finish: dev switched to prod for deploy, project finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DevService } from './data/dev.service';
+//import { DevService } from './data/dev.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { AuthModule } from './auth/auth.module';
 import { ProdutoModule } from './produto/produto.module';
+import { ProdService } from './data/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule]
     }),
     UsuarioModule,

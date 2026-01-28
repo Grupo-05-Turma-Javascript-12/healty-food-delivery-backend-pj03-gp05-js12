f3fece962e878138fcd240fe97a35e5ff5303d3f
João Henrique <jhenrique.caval@hotmail.com>
2026-01-28 11:41:19 -0300

    feat: ajustando imports

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdutoController } from './controllers/produto.controller';
 import { Produto } from './entities/produto.entity';
 import { ProdutoService } from './services/produto.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Produto])],
   providers: [ProdutoService],
-  controllers: [],
-  exports: [],
+  controllers: [ProdutoController],
+  exports: [ProdutoService],
 })
 export class ProdutoModule {}

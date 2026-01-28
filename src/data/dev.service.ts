a543ecae26915bfdcd01095bae0ce63c0ef24792
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-28 10:54:13 -0300

    feat: app module and controller updated

--- src/data/dev.service.ts
+++ src/data/dev.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { Categoria } from '../../categoria/entities/categoria.entity';
-import { Produto } from '../../produto/entities/produto.entity';
-import { Usuario } from '../../usuario/entities/usuario.entity';
+import { Produto } from '../produto/entities/produto.entity';
+import { Categoria } from '../categoria/entities/categoria.entity';
+import { Usuario } from '../usuario/entities/user.entity';
 
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {

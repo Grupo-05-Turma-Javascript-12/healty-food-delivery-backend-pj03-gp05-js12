a586cc675227dcd534b89ad06b0c26483df04b26
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-05-25 22:17:28 -0300

    fix(auth): use authenticated user from local guard II

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,7 +2,6 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsuarioService } from '../../usuario/services/user.service';
 import { Bcrypt } from '../bcrypt/bcrypt';
-import { UserLogin } from '../entities/userlogin.entity';
 
 @Injectable()
 export class AuthService {

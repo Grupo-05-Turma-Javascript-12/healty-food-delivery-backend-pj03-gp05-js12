d2f6968c6f1c31f0a63aff043cfae5cab5709ff9
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-05-25 23:47:06 -0300

    fix: product entity fixed

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,6 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
 import { Categoria } from '../../categoria/entities/categoria.entity';
 import { Usuario } from '../../usuario/entities/user.entity';
 
@@ -16,25 +24,33 @@ export class Produto {
   nome: string;
 
   @ApiProperty()
+  @IsOptional()
   @Column({ length: 500, nullable: true })
   descricao: string;
 
   @ApiProperty()
-  @IsNotEmpty()
+  @IsNumber()
   @Column('decimal', { precision: 10, scale: 2, nullable: false })
   preco: number;
 
   @ApiProperty()
+  @IsBoolean()
   @Column({ default: true })
   em_estoque: boolean;
 
   @ApiProperty({ type: () => Categoria })
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => Categoria)
   @ManyToOne(() => Categoria, (categoria) => categoria.produtos, {
     onDelete: 'SET NULL',
   })
   categoria: Categoria;
 
   @ApiProperty({ type: () => Usuario })
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => Usuario)
   @ManyToOne(() => Usuario, (usuario) => usuario.produtos, {
     onDelete: 'CASCADE',
   })
